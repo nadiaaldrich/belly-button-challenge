5cb1b04840a2cd014f2c828ea2d810f01a5db17f
N Aldrich <naldrich@hs-27306.local>
2021-06-18 12:40:47 -0500

    saving

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,3 +1,11 @@
-function buildMetadata(sample) {
-    d3.json("sample.json")
+function getPlots(id){
+    // read sample
+    d3.json("samples.json").then(sampledata => {
+        console.log(sampledata)
+        var ids = sampledata.samples[0].otu_ids;
+        console.log(ids)
+        var sampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
+        console.log(sampleValues)
+        var labels = sampledata.samples[0].
+    })
 }
